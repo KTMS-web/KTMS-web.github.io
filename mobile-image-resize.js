e6aeeca56e4b21985132405a3e955bad79d0e987
dr-data <data@connect.hku.hk>
2020-06-03 03:34:22 +0800

    cleaned code & removed all comment

--- mobile-image-resize.js
+++ mobile-image-resize.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 const fs = require('fs');
-const directory = './assets/image/brands';
+const directory = 'https://cdn.jsdelivr.net/gh/KTMS-web/KTMS-web.github.io/https://cdn.jsdelivr.net/gh/KTMS-web/KTMS-web.github.io/assets/image/brands';
 
 fs.readdirSync(directory).forEach(file => {
   sharp(`${directory}/${file}`)
